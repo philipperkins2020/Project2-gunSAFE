0d6f7ae5f34d860ef8aedcb54e1133d60789b239
Javan <71285290+djavanw@users.noreply.github.com>
2021-05-12 20:34:39 -0600

    Changed inventory to personal. References personal inventory of user.

--- seeds/personal_seeds.js
+++ seeds/personal_seeds.js
@@ -1,6 +1,6 @@
-const { Inventory } = require("../models");
+const { Personal } = require("../models");
 
-const inventoryData = [
+const personalData = [
     {
         manufacturer: "Bushmaster",
         model: "M4-A3",
@@ -8,6 +8,7 @@ const inventoryData = [
         serial_number: "987-C8-B1969",
         caliber: 5.56,
         barrel_len: 16,
+        action: "Semi",
         purchase_date: "6 Apr 2010",
         sloc: "Safe",
         cost: 799.99,
@@ -19,6 +20,7 @@ const inventoryData = [
         serial_number: "123-8A-D1988",
         caliber: 44,
         barrel_len: 6,
+        action: "Single/Double", 
         purchase_date: "12 Jun 2008",
         sloc: "Safe",
         cost: 1582.99,
@@ -30,6 +32,7 @@ const inventoryData = [
         serial_number: "769-D6-R1889",
         caliber: 9,
         barrel_len: 3,
+        action: "Single",
         purchase_date: "29 Oct 2020",
         sloc: "Safe",
         cost: 1249.00,
@@ -41,6 +44,7 @@ const inventoryData = [
         serial_number: "424-8A-V4309",
         caliber: "308 Win",
         barrel_len: 20,
+        action: "Bolt",
         purchase_date: "13 Feb 2017",
         sloc: "Gun Rack",
         cost: 1599.99,
@@ -48,6 +52,6 @@ const inventoryData = [
 
 ];
 
-const seedInventories = () => Inventory.bulkCreate(inventoryData);
+const seedPersonals = () => Personal.bulkCreate(personalData);
 
-module.exports = seedInventories;
+module.exports = seedPersonals;
