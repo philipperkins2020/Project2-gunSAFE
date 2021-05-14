c4900bcafb41e680ecf6d1ea0e6d754a341deb27
Bryan Gee <begee217@gmail.com>
2021-05-13 21:47:34 -0500

    added to category names

--- seeds/category_seeds.js
+++ seeds/category_seeds.js
@@ -1,24 +1,39 @@
-const { Category } = require('../models');
+const { Category } = require("../models");
 
 const categoryData = [
-    {
-        category_name: 'Semi-Automatic Rifle',
-    },
-    {
-        category_name: 'Bolt Action Rifle',
-    },
-    {
-        category_name: 'Lever Action Rifle',
-    },
-    {
-        category_name: 'Shotgun',
-    },
-    {
-        category_name: 'Semi-Automatic Handgun/Pistol',
-    },
-    {
-        category_name: 'Revolver',
-    },
+  {
+    category_name: "Semi-Automatic Rifle",
+  },
+  {
+    category_name: "Bolt Action Rifle",
+  },
+  {
+    category_name: "Lever Action Rifle",
+  },
+  {
+    category_name: "Pump Action Rifle",
+  },
+  {
+    category_name: "Pump Action Shotgun",
+  },
+  {
+    category_name: "Lever Action Shotgun",
+  },
+  {
+    category_name: "Semi-Automatic Shotgun",
+  },
+  {
+    category_name: "Semi-Automatic Handgun/Pistol",
+  },
+  {
+    category_name: "Revolver",
+  },
+  {
+    category_name: "Derringer",
+  },
+  {
+    category_name: "Bolt Action Handgun/Pistol",
+  },
 ];
 
 const seedCategory = () => Category.bulkCreate(categoryData);
