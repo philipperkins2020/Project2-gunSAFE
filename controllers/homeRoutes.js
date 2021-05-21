c418873a90954ae237aaf8613072bcd01a9a7a7d
Bryan Gee <begee217@gmail.com>
2021-05-21 18:19:32 -0500

    added safety page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,54 +1,57 @@
-const router = require('express').Router(); 
+const router = require("express").Router();
 
-const { User, Personal } = require('../models');
+const { User, Personal } = require("../models");
 
-router.get('/', (req, res) => {
-    try {
-        res.render('homepage', {
-            // logged_in: req.seesion.logged_in,
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+router.get("/", (req, res) => {
+  try {
+    res.render("homepage", {
+      // logged_in: req.seesion.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-
-router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-      res.redirect('/mysafe');
-      return;
-    }
-    res.render('login');
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/mysafe");
+    return;
+  }
+  res.render("login");
 });
 
-
-router.get('/newuser', (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect('/mySafe');
-        return;
-    }
-    res.render('newuser');
+router.get("/newuser", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/mySafe");
+    return;
+  }
+  res.render("newuser");
 });
 
-router.get('/logout', (req, res) => {
-    if (req.session.logged_in) {
-        req.session.destroy(() => {
-            res.status(204).redirect('/');
-        });
-    }
-    else {
-        res.status(204).redirect('/');
-    }
+router.get("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).redirect("/");
+    });
+  } else {
+    res.status(204).redirect("/");
+  }
 });
 
-router.get('/addinventory', (req, res) => {
-    if (!req.session.logged_in) {
-      res.redirect('/');
-      return;
-    }
-    res.render('addinventory');
+router.get("/addinventory", (req, res) => {
+  if (!req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("addinventory");
 });
 
+router.get("/safety", (req, res) => {
+  try {
+    res.render("safety");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-
-module.exports = router
+module.exports = router;
